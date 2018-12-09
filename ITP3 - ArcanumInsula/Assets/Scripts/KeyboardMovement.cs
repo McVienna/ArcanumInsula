36ed12efab16678b730968fa174023e7416fa7d3
McVienna <markus.vogl96@hotmail.com>
2018-12-09 23:37:10 +0100

    Added Scripts for PlayerMovement in SampleScene

---
+++ ITP3 - ArcanumInsula/Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour {
+
+    public Vector3 Jump;
+    public float JumpForce = 2;
+
+    public bool IsGrounded;
+
+    Rigidbody Rig;
+
+    void Start()
+    {
+        Rig = GetComponent<Rigidbody>();
+        Jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        IsGrounded = true;
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+
+        if(Input.GetKey("space") && IsGrounded)
+        {
+            Rig.AddForce(Jump * JumpForce, ForceMode.Impulse);
+            IsGrounded = false;
+        }
+
+    }
+}
