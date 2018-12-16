ff0162d370da1f99d142834e855cc3bf1c0b3cf6
McVienna <markus.vogl96@hotmail.com>
2018-12-17 00:56:25 +0100

    merged miniMap intor Terrain/added Player Mesh/Added PlayerAnimation

---
+++ ITP3 - ArcanumInsula/Assets/TEST/Scripts/AnimationTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+
+    // Use this for initialization
+    private GameObject m_animatedPlayer;
+    private Animator m_animator;
+    private Transform m_playerTransform;
+    private Vector3 m_currPos;
+    private Vector3 m_prevPos;
+
+
+
+    void Start()
+    {
+        m_animatedPlayer = GameObject.FindGameObjectWithTag("player_anim");
+        m_animator = m_animatedPlayer.GetComponent <Animator> ();
+        m_currPos = new Vector3(0, 0, 0);
+        m_prevPos = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_playerTransform = GetComponent<Transform>();
+
+        m_currPos = m_playerTransform.position;
+
+        if (m_currPos == m_prevPos)
+        {
+            m_animator.SetBool("IsMoving", false);
+        }
+        
+        if(m_currPos != m_prevPos)
+        {
+            m_animator.SetBool("IsMoving", true);
+        }
+
+        m_prevPos = m_currPos;
+
+    }
+}
