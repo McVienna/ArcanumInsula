ba9bc14a4eecc02237d3fb198c8a22acd4076238
ElPichichi <h.h.koerner@live.de>
2019-01-15 03:01:57 +0100

    added Sound files

--- ITP3 - ArcanumInsula/Assets/TEST/Scripts/AnimationTrigger.cs
+++ ITP3 - ArcanumInsula/Assets/TEST/Scripts/AnimationTrigger.cs
@@ -12,12 +12,18 @@ public class AnimationTrigger : MonoBehaviour
     private Vector3 m_currPos;
     private Vector3 m_prevPos;
 
+    public AudioClip footsteps_sound;
+    public AudioSource footstep_source;
+
     void Start()
     {
         m_animatedPlayer = GameObject.FindGameObjectWithTag("player_anim");
         m_animator = m_animatedPlayer.GetComponent <Animator> ();
         m_currPos = new Vector3(0, 0, 0);
         m_prevPos = new Vector3(0, 0, 0);
+
+        footstep_source = this.gameObject.GetComponent<AudioSource>();
+        footstep_source.clip = footsteps_sound;
     }
 
     // Update is called once per frame
@@ -30,11 +36,15 @@ public class AnimationTrigger : MonoBehaviour
         if (m_currPos == m_prevPos)
         {
             m_animator.SetBool("IsMoving", false);
+
+            footstep_source.Stop();
         }
         
         if(m_currPos != m_prevPos)
         {
             m_animator.SetBool("IsMoving", true);
+
+            footstep_source.Play();
         }
 
         m_prevPos = m_currPos;
---
+++ ITP3 - ArcanumInsula/Assets/TEST/Scripts/footsteps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footsteps : MonoBehaviour {
+
+    public AudioClip footsteps_sound;
+    public AudioSource footstep_source;
+    CharacterController cc;
+
+    // Use this for initialization
+    void Start()
+    {
+        footstep_source = this.gameObject.GetComponent<AudioSource>();
+        footstep_source.clip = footsteps_sound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f)
+        {
+            footstep_source.Play();
+        }
+
+    }
+}
+
